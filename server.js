fc318b15365902e8d510c15b792c20d730e90e11
dwaynediddy <burrows.dwayne@icloud.com>
2021-02-02 10:40:25 +1000

    middleware basics

--- server.js
+++ server.js
@@ -1,39 +1,56 @@
 const express = require('express');
 const app = express();
 const hello = require('./hello.js')
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 5000
 
 // app.get('/', function(req, res) {
 //     res.send('GET route on hello')
 // })
 
 app.get('/example', function(req, res) {
+    //res send sends the example to the UI
     res.send('example route')
 })
 
 app.get('/example/A', function(req, res) {
-    res.send('example route')
+    res.send('example route two')
 })
 
 app.get('/example/B', function(req, res) {
-    res.send('example route')
+    res.send('example route three')
 })
 
-const callbackOne = function(req, res) {
-    console.log('example one')
+const callbackOne = function(req, res, next) {
+    console.log('callbackOne')
+    //next fires off the next function after current is done
     next()
 }
 
-const callbackTwo = function(req, res) {
-    console.log('example one')
+const callbackTwo = function(req, res, next) {
+    console.log('callbackTwo')
     next()
 }
 
 const callbackThree = function(req, res) {
-    console.log('example one')
-    next()
+    console.log('call backs triggered')
+    res.send('callbacks triggered!')
 }
 
-app.listen(5000, () => {
-    console.log(hello)
+app.get(
+    '/example/d',
+    function(req, res, next) {
+        console.log('teh response will be sent by the next function ...')
+        next()
+    },
+        function(req, res) {
+            res.send('hello from D!')
+    })
+
+    app.get(
+        '/example/C/withmiddleware', 
+        [callbackOne, callbackTwo, callbackThree]
+    ) 
+
+app.listen(PORT, () => {
+    console.log(`listening on port: ${PORT}`)
 })
