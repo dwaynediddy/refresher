613e908394476448953952fe97a00cc068f5e4b6
dwaynediddy <burrows.dwayne@icloud.com>
2021-02-04 14:11:53 +1000

    node pathing

---
+++ path.js
@@ -0,0 +1,7 @@
+var path = require('path')
+
+console.log('basename : ', path.basename(__filename))
+
+console.log('dirname: ', path.dirname(__filename))
+
+console.log('extname : ', path.extname(__filename))
