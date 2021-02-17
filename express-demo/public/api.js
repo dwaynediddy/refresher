eb0b454ad8b74d9819147c7d6899e17d2935357a
dwaynediddy <burrows.dwayne@icloud.com>
2021-02-17 19:50:41 +1000

    trial

--- express-demo/public/api.js
+++ express-demo/public/api.js
@@ -7,7 +7,35 @@ function getRequest(event) {
     .then(function(response) {
         return response.json()
     })
-        .then(function(response) {
-            console.log(JSON.stringify(response))
+        .then(function(data) {
+            for(var i in data) {
+                document.getElementById('results').innerHTML += data[i].movieTitle + '<br />'
+                
+            }
+            console.log(JSON.stringify(data))
         })
+}
+
+var postButton = document.getElementById('user_form_post')
+postButton.addEventListener('submit', newPost)
+
+function newPost(event, post) {
+    event.preventDefault()
+    var movieTitle = event.target.movieTitle.value
+    var movieDirector = event.target.movieDirector.value
+    post = {
+        movieTitle: movieTitle,
+        movieDirector: movieDirector
+    }
+    const options = {
+        method: 'POST',
+        body: JSON.stringify(post),
+        headers: new Headers({ 
+            'Content-Type': 'application/json'
+        })
+    }
+    return fetch('/movies', options)
+    .then(res => res.json())
+    .then(res => console_log(res))
+    .then(error => console_log('error: ', error))
 }
